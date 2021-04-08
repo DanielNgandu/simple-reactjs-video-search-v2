d4770b5f426db4769689ad2dbe526297de7578c1
DNg'andu <daniel.ngandu@brhc.com>
2021-04-08 12:19:16 +0200

    make reusable hook.

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import fetchVideos from '../hooks/fetchVideos';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
+  const [videos, search] = fetchVideos('cars');
   const [selectedVideo, setSelectedVideo] = useState(null);
 
   useEffect(() => {
-    onTermSubmit('buildings');
-  }, []);
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term,
-      },
-    });
 
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
-
-  // const onVideoSelect = (video) => {
-  //   setSelectedVideo(video);
-  // };
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
